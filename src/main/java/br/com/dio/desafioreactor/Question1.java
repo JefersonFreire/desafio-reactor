a049e54d950f6110957dbe57584f3324d8fba29e
jeferson freire <jfreire86@hotmail.com>
2024-10-29 17:34:22 -0300

    entrega do desafio

--- src/main/java/br/com/dio/desafioreactor/Question1.java
+++ src/main/java/br/com/dio/desafioreactor/Question1.java
@@ -4,11 +4,17 @@ import reactor.core.publisher.Flux;
 
 import java.util.List;
 
+
 public class Question1 {
 
     /*
     Recebe uma lista de longs, incrementa 1 nos valores e retorna um flux dos resultados
      */
-    public Flux<Long> inc(final List<Long> numbers){}
+    public Flux<Long> inc(final List<Long> numbers){
+        return Flux.fromIterable(numbers)
+                .map(n -> n + 1L);
+
+    }
+
 
 }
